afe19422db004ec4f83ca2c10519219283167e31
EVAN <jnswcy@gmail.com>
2016-12-08 11:14:43 +0800

    完成

--- InformationActivity.java
+++ InformationActivity.java
@@ -30,14 +30,6 @@ public class InformationActivity extends AppCompatActivity implements Navigation
 
         initImageView();
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
